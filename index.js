a362648395e8142459405446f987e4a3c3675c9e
mikiyoshi <mikiyoshi.kokura@gmail.com>
2022-01-17 05:07:37 +0000

    add new function

--- index.js
+++ index.js
@@ -23,39 +23,3 @@ function sum(fromN, toN) {
 }
 console.log(sum(3, 7));
 module.exports = sum;
-
-// https://github.com/ZSully09/Week-1/blob/163a5053f656d55981b9d2c202f4c3c872afa3b2/d5-FF/recursion_clone/index.js
-// function sum(fromN, toN) {
-//   // Sum all the values from fromN up to toN [3 --> 7]
-//   // Add base Case
-//   if (fromN === toN) {
-//     return fromN;
-//   }
-//   // Add Recursive Case
-//   return fromN + sum(fromN + 1, toN);
-// }
-
-// console.log(sum(3, 7));
-
-// module.exports = sum;
-
-// /*
-// Base Case: fromN === toN
-// Recursive Case: fromN + sum(fromN + 1, toN)
-// */
-
-// // Sum all the values of toN to fromN [ 7 --> 3]
-
-// function sum(fromN, toN) {
-//   if (fromN === toN) {
-//     return fromN;
-//   }
-//   return toN + sum(fromN, toN - 1);
-// }
-
-// console.log(sum(3, 7));
-
-/*
-Base Case: fromN === toN
-Recursive Case: toN + sum(fromN, toN - 1)
-*/
